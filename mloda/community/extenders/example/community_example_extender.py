4a75a3b9fcad956f9293c474edc1e6f6a41a8352
Tom Kaltofen <tomkaltofen@gmail.com>
2026-08-10 09:57:01 +0200

    fix: project release

--- mloda/community/extenders/example/community_example_extender.py
+++ mloda/community/extenders/example/community_example_extender.py
@@ -19,3 +19,4 @@ class CommunityExampleExtender(Extender):
     def __call__(self, func: Any, *args: Any, **kwargs: Any) -> Any:
         """Execute the wrapped function without modification."""
         return func(*args, **kwargs)
+
